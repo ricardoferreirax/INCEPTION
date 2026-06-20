527923617b50fc1deddc951ed930b49ea66bcc35
Ricardo Medeiros <ricardomedeirosx@gmail.com>
2026-06-20 22:59:42 +0100

    Change and perfect nginx

--- srcs/requirements/wordpress/tools/init_wordpress.sh
+++ srcs/requirements/wordpress/tools/init_wordpress.sh
@@ -4,6 +4,7 @@ set -e
 
 WORDPRESS_DIR="/var/www/html"
 WP_CONFIG_FILE="$WORDPRESS_DIR/wp-config.php"
+
 PHP_FPM_RUN_DIR="/run/php"
 PHP_FPM_CONFIG_DIR="/etc/php/8.2/fpm/pool.d"
 PHP_FPM_CONFIG_FILE="$PHP_FPM_CONFIG_DIR/www.conf"
@@ -31,18 +32,28 @@ else
 fi
 
 echo "[WORDPRESS] >> Checking required environment variables..."
-if [ -z "$MDB_HOST" ] || [ -z "$MDB_PORT" ] || [ -z "$MDB_DATABASE" ] || [ -z "$MDB_USER" ] || \
-	[ -z "$WP_URL" ] || [ -z "$WP_TITLE" ] || [ -z "$WP_ADMIN_USER" ] || \
-	[ -z "$WP_ADMIN_EMAIL" ] || [ -z "$WP_USER" ] || [ -z "$WP_USER_EMAIL" ] || \
-	[ -z "$WP_USER_ROLE" ] || [ -z "$PHP_FPM_PORT" ];
+if [ -z "$DOMAIN_NAME" ] || [ -z "$NGINX_PORT" ] || \
+	[ -z "$MDB_HOST" ] || [ -z "$MDB_PORT" ] || \
+	[ -z "$MDB_DATABASE" ] || [ -z "$MDB_USER" ] || \
+	[ -z "$WP_TITLE" ] || [ -z "$WP_ADMIN_USER" ] || \
+	[ -z "$WP_ADMIN_EMAIL" ] || [ -z "$WP_USER" ] || \
+	[ -z "$WP_USER_EMAIL" ] || [ -z "$WP_USER_ROLE" ] || \
+	[ -z "$PHP_FPM_PORT" ];
 then
 	echo "[ERROR] >> Required environment variables are missing."
 	exit 1
 fi
 
-if ! [[ "$MDB_PORT" =~ ^[0-9]+$ ]] || ! [[ "$PHP_FPM_PORT" =~ ^[0-9]+$ ]]; then
-	echo "[ERROR] >> MDB_PORT and PHP_FPM_PORT must be numbers."
+if ! [[ "$MDB_PORT" =~ ^[0-9]+$ ]] || ! [[ "$PHP_FPM_PORT" =~ ^[0-9]+$ ]] || ! [[ "$NGINX_PORT" =~ ^[0-9]+$ ]];
+then
+	echo "[ERROR] >> MDB_PORT, PHP_FPM_PORT and NGINX_PORT must be numbers."
 	exit 1
+fi
+
+if [ "$NGINX_PORT" = "443" ]; then
+	WP_FULL_URL="https://${DOMAIN_NAME}"
+else
+	WP_FULL_URL="https://${DOMAIN_NAME}:${NGINX_PORT}"
 fi
 
 mkdir -p "$WORDPRESS_DIR"
@@ -51,9 +62,6 @@ mkdir -p "$PHP_FPM_CONFIG_DIR"
 
 chown -R www-data:www-data "$WORDPRESS_DIR" "$PHP_FPM_RUN_DIR"
 
-# Creates the PHP-FPM pool configuration file with the necessary settings for the www pool. The configuration specifies 
-# that the pool listens on all interfaces on the specified port, runs as the www-data user and group, and uses dynamic 
-# process management with defined limits for child processes.
 echo "[WORDPRESS] >> Creating PHP-FPM configuration file..."
 cat > "$PHP_FPM_CONFIG_FILE" << EOF
 [www]
@@ -70,11 +78,8 @@ pm.max_spare_servers = 3
 clear_env = no
 EOF
 
-# Enter the WordPress directory to execute WP-CLI commands, which is a tool for managing WordPress installations.
 cd "$WORDPRESS_DIR"
 
-# Attempts to connect to the MariaDB server in a loop until it is ready to accept connections. 
-# This ensures that the database is available before proceeding with WordPress installation.
 echo "[WORDPRESS] >> Waiting for MariaDB..."
 until mariadb -h "$MDB_HOST" -P "$MDB_PORT" -u "$MDB_USER" -p"$DB_PASSWORD" "$MDB_DATABASE" -e "SELECT 1" >/dev/null 2>&1
 do
@@ -87,15 +92,20 @@ if [ -f "$WP_CONFIG_FILE" ]; then
 
 	echo "[WORDPRESS] >> Updating database host configuration..."
 	wp config set DB_HOST "${MDB_HOST}:${MDB_PORT}" --allow-root
+
+	echo "[WORDPRESS] >> Updating WordPress site URL..."
+	wp option update siteurl "$WP_FULL_URL" --allow-root
+	wp option update home "$WP_FULL_URL" --allow-root
 else
 	echo "[WORDPRESS] >> Downloading WordPress core files..."
 	wp core download --allow-root
 
 	echo "[WORDPRESS] >> Creating wp-config.php..."
-	wp config create --dbname="$MDB_DATABASE" --dbuser="$MDB_USER" --dbpass="$DB_PASSWORD" --dbhost="${MDB_HOST}:${MDB_PORT}" --allow-root
+	wp config create --dbname="$MDB_DATABASE" --dbuser="$MDB_USER" --dbpass="$DB_PASSWORD" --dbhost="${MDB_HOST}:${MDB_PORT}" \
+		--allow-root
 
 	echo "[WORDPRESS] >> Installing WordPress site..."
-	wp core install --url="$WP_URL" --title="$WP_TITLE" --admin_user="$WP_ADMIN_USER" --admin_password="$WP_ADMIN_PASSWORD" \
+	wp core install --url="$WP_FULL_URL" --title="$WP_TITLE" --admin_user="$WP_ADMIN_USER" --admin_password="$WP_ADMIN_PASSWORD" \
 		--admin_email="$WP_ADMIN_EMAIL" --skip-email --allow-root
 
 	echo "[WORDPRESS] >> Creating additional WordPress user..."
