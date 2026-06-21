a746c198336712c561d6fe0f004ffa4b5cd4b845
Ricardo Medeiros <ricardomedeirosx@gmail.com>
2026-06-21 23:47:10 +0100

    Test FTP connection

--- srcs/requirements/bonus/ftp/tools/init_ftp.sh
+++ srcs/requirements/bonus/ftp/tools/init_ftp.sh
@@ -4,7 +4,6 @@ set -e
 
 FTP_ROOT_DIR="/var/www/html"
 FTP_CONFIG_FILE="/etc/vsftpd.conf"
-FTP_USERLIST_FILE="/etc/vsftpd.userlist"
 FTP_PASSWORD_FILE="/run/secrets/ftp_password"
 
 echo "[FTP] >> Verifying required Docker secrets..."
@@ -24,18 +23,20 @@ fi
 mkdir -p "$FTP_ROOT_DIR"
 mkdir -p /var/run/vsftpd/empty
 
-echo "[FTP] >> Creating FTP user..."
-if ! id "$FTP_USER" >/dev/null 2>&1; then
-	useradd -m -d "$FTP_ROOT_DIR" -s /bin/bash "$FTP_USER"
+echo "[FTP] >> Preparing FTP user..."
+if ! getent group www-data >/dev/null 2>&1; then
+	groupadd -g 33 www-data
 fi
 
+if ! id "$FTP_USER" >/dev/null 2>&1; then
+	useradd -u 33 -g www-data -d "$FTP_ROOT_DIR" -s /bin/bash "$FTP_USER"
+fi
+
+echo "[FTP] >> Setting FTP user password..."
 echo "$FTP_USER:$FTP_PASSWORD" | chpasswd
 
-echo "[FTP] >> Creating vsftpd user list..."
-echo "$FTP_USER" > "$FTP_USERLIST_FILE"
-
 echo "[FTP] >> Updating FTP root ownership..."
-chown -R "$FTP_USER:$FTP_USER" "$FTP_ROOT_DIR"
+chown -R www-data:www-data "$FTP_ROOT_DIR"
 chmod -R 755 "$FTP_ROOT_DIR"
 
 echo "[FTP] >> Creating vsftpd configuration file..."
@@ -47,12 +48,12 @@ local_enable=YES
 write_enable=YES
 local_umask=022
 chroot_local_user=YES
-pasv_address=${DOMAIN_NAME}
 allow_writeable_chroot=YES
 local_root=${FTP_ROOT_DIR}
 pasv_enable=YES
 pasv_min_port=${FTP_PASV_MIN_PORT}
 pasv_max_port=${FTP_PASV_MAX_PORT}
+pasv_address=127.0.0.1
 EOF
 
 echo "[FTP] >> Starting vsftpd in foreground..."
