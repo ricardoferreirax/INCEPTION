ab377b73c4a30360bb959ae36d6aab143e14f57e
Ricardo Medeiros <ricardomedeirosx@gmail.com>
2026-09-07 17:09:45 +0100

    fix and document nginx script file

--- srcs/requirements/nginx/tools/init_nginx.sh
+++ srcs/requirements/nginx/tools/init_nginx.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-set -e
+# stop immediately if any command fails and if an undefined variable is used.
+set -eu
 
 NGINX_SSL_DIR="/etc/nginx/ssl"
 NGINX_CONFIG_DIR="/etc/nginx/conf.d"
@@ -8,29 +9,27 @@ NGINX_CONFIG_FILE="$NGINX_CONFIG_DIR/default.conf"
 NGINX_SSL_CERT="$NGINX_SSL_DIR/inception.crt"
 NGINX_SSL_KEY="$NGINX_SSL_DIR/inception.key"
 
-echo "[NGINX] >> Checking required environment variables..."
-if [ -z "$DOMAIN_NAME" ] || [ -z "$PHP_FPM_HOST" ] || [ -z "$PHP_FPM_PORT" ]; then
-	echo "[ERROR] >> DOMAIN_NAME, PHP_FPM_HOST or PHP_FPM_PORT is missing."
-	exit 1
-fi
-
-if ! [[ "$PHP_FPM_PORT" =~ ^[0-9]+$ ]]; then
-	echo "[ERROR] >> PHP_FPM_PORT must be a number."
-	exit 1
-fi
-
 mkdir -p "$NGINX_SSL_DIR"
 mkdir -p "$NGINX_CONFIG_DIR"
 
-echo "[NGINX] >> Creating SSL certificate..."
+echo "[NGINX] >> Checking SSL certificate..."
+
+# create a self-signed certificate only if the certificate or its private key doesn't already exist.
+# NGINX will use this certificate to provide HTTPS connections on port 443.
 if [ ! -f "$NGINX_SSL_CERT" ] || [ ! -f "$NGINX_SSL_KEY" ]; then
-	openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout "$NGINX_SSL_KEY" -out "$NGINX_SSL_CERT" \
-		-subj "/C=PT/ST=Lisbon/L=Lisbon/O=42/OU=Inception/CN=${DOMAIN_NAME}"
+	echo "[NGINX] >> Creating SSL certificate..."
+
+	openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout "$NGINX_SSL_KEY" -out "$NGINX_SSL_CERT" -subj "/C=PT/ST=Lisbon/L=Lisbon/O=42/OU=Inception/CN=${DOMAIN_NAME}"
+
 else
 	echo "[NGINX] >> SSL certificate already exists. Reusing existing certificate."
+
 fi
 
 echo "[NGINX] >> Creating NGINX configuration file..."
+
+# create the NGINX server config, NGINX listens only on HTTPS port 443, only TLS 1.2 and TLS 1.3 are enabled.
+# static WordPress files are served directly by NGINX, PHP files are forwarded to PHP-FPM running inside the WordPress container.
 cat > "$NGINX_CONFIG_FILE" << EOF
 server 
 {
@@ -38,20 +37,15 @@ server
 	listen [::]:443 ssl;
 
 	server_name ${DOMAIN_NAME};
-
 	root /var/www/html;
 	index index.php index.html;
-
 	ssl_certificate ${NGINX_SSL_CERT};
 	ssl_certificate_key ${NGINX_SSL_KEY};
-
 	ssl_protocols TLSv1.2 TLSv1.3;
-
 	location / 
 	{
 		try_files \$uri \$uri/ /index.php?\$args;
 	}
-
 	location ~ \.php$ 
 	{
 		include fastcgi_params;
@@ -63,5 +57,12 @@ server
 }
 EOF
 
+echo "[NGINX] >> Testing NGINX configuration..."
+
+# validate the generated config before starting NGINX.
+nginx -t
+
 echo "[NGINX] >> Starting NGINX in foreground..."
+
+# daemon off keeps NGINX running in the foreground, exec replaces this Bash script with the NGINX process, making NGINX PID 1 inside the container.
 exec nginx -g "daemon off;"
