317f60ee5681a6abc7d30a5b58b6da074f38502e
Ricardo Medeiros <ricardomedeirosx@gmail.com>
2026-09-09 05:50:43 +0100

    Refactor redis init script and improve startup logs

--- srcs/requirements/bonus/redis/tools/init_redis.sh
+++ srcs/requirements/bonus/redis/tools/init_redis.sh
@@ -7,9 +7,8 @@ REDIS_CONFIG_FILE="/etc/redis/redis.conf"
 
 echo "[REDIS] >> Creating Redis configuration file..."
 
-# redis listens on all interfaces inside the Docker network, port 6379 is only exposed internally to the other containers.
-# redis runs in foreground so it can be managed directly by Docker. 
-# Persistence is disabled because Redis is used only as a cache for the WordPress service.
+# redis listens on all interfaces inside the Docker network and runs in foreground so it can be managed directly by Docker. 
+# persistence is disabled because Redis is used only as a cache for the WordPress service.
 cat > "$REDIS_CONFIG_FILE" << EOF
 bind 0.0.0.0
 port 6379
@@ -18,8 +17,9 @@ daemonize no
 save ""
 appendonly no
 EOF
+echo "[REDIS] >> Redis configuration created successfully."
 
 echo "[REDIS] >> Starting Redis in foreground..."
+echo "[REDIS] >> Current Bash PID: $$"
 
-# exec replaces the Bash script with redis-server, making Redis PID 1 inside the container.
 exec redis-server "$REDIS_CONFIG_FILE"
