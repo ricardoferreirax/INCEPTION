c0accd7533330b3d74a5536dfbd0a09e4aae0442
Ricardo Medeiros <ricardomedeirosx@gmail.com>
2026-09-10 16:59:43 +0100

    Static page service is working good. Made some changes on Dockerfile and config file

--- srcs/requirements/bonus/static/tools/script.js
+++
@@ -1,9 +0,0 @@
-function showArchitecture()
-{
-    const architecture = document.getElementById("architecture");
-
-    if (architecture.style.display === "block")
-        architecture.style.display = "none";
-    else
-        architecture.style.display = "block";
-}
