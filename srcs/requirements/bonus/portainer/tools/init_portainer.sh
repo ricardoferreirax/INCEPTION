3db26355d75c1b24258eadc785e2d7b981e9a6ea
Ricardo Medeiros <ricardomedeirosx@gmail.com>
2026-09-16 23:59:24 +0100

    Resolve portainer

---
+++ srcs/requirements/bonus/portainer/tools/init_portainer.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Stop immediately if any command fails or if an undefined variable is used.
+set -eu
+
+PORTAINER_DATA_DIR="/data"
+
+# Create the directory where Portainer stores its persistent data.
+mkdir -p "$PORTAINER_DATA_DIR"
+
+echo "[PORTAINER] >> Starting Portainer in foreground..."
+echo "[PORTAINER] >> Current Bash PID: $$"
+
+exec /opt/portainer/portainer --data "$PORTAINER_DATA_DIR" --http-enabled
