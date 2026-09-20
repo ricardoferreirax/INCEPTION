f79171b15dc9947096513c447dbf94140f4cecc1
Ricardo Medeiros <ricardomedeirosx@gmail.com>
2026-09-20 21:43:35 +0100

    Clean adminer and optimise script

--- srcs/requirements/bonus/adminer/tools/init_adminer.sh
+++ srcs/requirements/bonus/adminer/tools/init_adminer.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 
-# Stop immediately if any command fails or if an undefined variable is used.
+# stop the script immediately if a command fails or if an undefined variable is used.
 set -eu
 
+# define path of the PHP file used to start the application.
 ADMINER_DIR="/var/www/html"
 ADMINER_FILE="$ADMINER_DIR/index.php"
 
+echo "[ADMINER] >> Creating directory where Adminer PHP file will be stored..."
 mkdir -p "$ADMINER_DIR"
 
+echo "[ADMINER] >> Checking if Adminer is already downloaded..."
 if [ ! -f "$ADMINER_FILE" ]; then
-    echo "[ADMINER] >> Adminer file not found. Downloading Adminer..."
+    echo "[ADMINER] >> Downloading Adminer..."
     curl -fsSL "https://github.com/vrana/adminer/releases/download/v6.0.2/adminer-6.0.2-mysql.php" -o "$ADMINER_FILE"
+
     echo "[ADMINER] >> Adminer downloaded successfully."
-else
-    echo "[ADMINER] >> Existing Adminer file found. Skipping download."
 fi
 
+echo "[ADMINER] >> Giving the web server user www-data ownership of the Adminer files..."
 chown -R www-data:www-data "$ADMINER_DIR"
 
-echo "[ADMINER] >> Starting Adminer server in foreground..."
-echo "[ADMINER] >> Current Bash PID: $$"
-
+echo "[ADMINER] >> Start Adminer PHP's built-in web server in foreground..."
+echo "[ADMINER] >> Allows connections from other containers and serves the Adminer PHP file..."
+echo "[ADMINER] >> Adminer is now accessible at https://${DOMAIN_NAME}/adminer/ in web browser."
 exec php -S "0.0.0.0:8080" -t "$ADMINER_DIR"
